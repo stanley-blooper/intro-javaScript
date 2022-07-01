b6342073766c7260ed4ff736e0e7db33583aaf20
stanley-blooper <60666266+stanley-blooper@users.noreply.github.com>
2022-06-30 23:22:56 -0400

    commit first

---
+++ index.js
@@ -0,0 +1,31 @@
+let person = {
+    name: 'Stanley',
+    age: 32,
+    selection: true
+}
+/*/object
+console.log(person)
+
+//dot notation
+console.log(person.name)
+
+//bracket notation
+console.log()*/
+
+//person['age']=100000;
+
+console.log(person.age);
+
+///arrays
+
+let selectedColors = ['blue','red' ];
+selectedColors[2]='purple';
+ console.log(selectedColors.length)
+ console.log(selectedColors[0]);
+
+ // function
+ function greet(){
+console.log('Hello World')
+
+ }
+ //function with 
